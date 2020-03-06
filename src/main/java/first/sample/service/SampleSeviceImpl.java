176cf7a94801e4d1b73e3b299233196916c1b24c
loverb3 <loverb3@hanmail.net>
2020-03-06 16:14:08 +0900

    첫번째 커밋

---
+++ src/main/java/first/sample/service/SampleSeviceImpl.java
@@ -0,0 +1,72 @@
+package first.sample.service;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import first.sample.dao.SampleDAO;
+
+@Service("sampleService")
+public class SampleSeviceImpl implements SampleService {
+	Logger log = Logger.getLogger(this.getClass());
+		
+	@Resource(name="sampleDAO")
+	private SampleDAO sampleDAO;
+
+	@Override
+	public List<Map<String, Object>> selectBoradList(Map<String, Object> map) throws Exception {
+		
+		return sampleDAO.selectBoardList(map);
+		
+	}
+	
+	@Override
+	public void insertBoard(Map<String, Object> map, HttpServletRequest request) throws Exception {
+		
+		sampleDAO.insertBoard(map);
+		MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest) request;
+		Iterator<String> iterator = multipartHttpServletRequest.getFileNames();
+		MultipartFile multipartFile = null;
+		while(iterator.hasNext()) {
+			multipartFile = multipartHttpServletRequest.getFile(iterator.next());
+			if(multipartFile.isEmpty() == false) {
+				log.debug("---------------- file start ----------------");
+				log.debug("name : " +multipartFile.getName());
+				log.debug("filename : " +multipartFile.getOriginalFilename());
+				log.debug("size : " +multipartFile.getSize());
+				log.debug("----------------  file end  ----------------");
+			}
+			
+		}
+		
+	}
+	
+	@Override
+	public Map<String, Object> selectBoardDetail(Map<String, Object> map) throws Exception {
+		sampleDAO.updateHitCnt(map);
+		Map<String, Object> resultMap = sampleDAO.selectBoardDetail(map);
+		return resultMap;
+	}
+	
+	@Override
+	public void updateBoard(Map<String, Object> map) throws Exception {
+		
+		sampleDAO.updateBoard(map);
+		
+	}
+	
+	@Override
+	public void deleteBoard(Map<String, Object> map) throws Exception {
+		
+		sampleDAO.deleteBoard(map);
+	}
+	
+}
